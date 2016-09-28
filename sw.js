17563778f27c72dbacaa280eada36e0b9ed95bb7
Julien Thoma <julien.thoma@reservix.de>
2016-09-28 16:01:09 +0200

    update + sw

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open('overheroes').then(function (cache) {
+    return cache.addAll(['/', '/index.html']);
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.open('overheroes').then(function (cache) {
+    return cache.match(event.request).then(function (response) {
+      return response || fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+
+            return response;
+          });
+    });
+  }));
+});
