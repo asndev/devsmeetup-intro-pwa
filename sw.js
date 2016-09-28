68856e7166629eb84aa8b1b6b11c74163028db62
Julien Thoma <julien.thoma@reservix.de>
2016-09-28 16:15:12 +0200

    update sw

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', function (event) {
-  event.waitUntil(caches.open('overheroes').then(function (cache) {
+  event.waitUntil(caches.open('swdev').then(function (cache) {
     return cache.addAll(['/', '/index.html']);
   }));
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.open('overheroes').then(function (cache) {
+  event.respondWith(caches.open('swdev').then(function (cache) {
     return cache.match(event.request).then(function (response) {
       return response || fetch(event.request).then(function (response) {
             cache.put(event.request, response.clone());
